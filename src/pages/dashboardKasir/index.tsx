9b8c1c85b01c6f132d2363145365546ddc03ef97
DifaaNug <123245678+DifaaNug@users.noreply.github.com>
2024-06-30 23:53:37 +0700

    router done

---
+++ src/pages/dashboardKasir/index.tsx
@@ -0,0 +1,12 @@
+import Kasir from '@/components/Kasir/page'
+import React from 'react'
+
+const index = () => {
+  return (
+    <main>
+        <Kasir/>
+    </main>
+  )
+}
+
+export default index
