989ddeba7a0f31c03ef6dfd898b57a1205bf072c
AhmadAzkia <ahmad.10122065@mahasiswa.unikom.ac.id>
2024-07-16 02:55:38 +0700

    membuat halaman login

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <><div>LoginPage</div><a href="../../components/Pelayan/dashboardPelayan">aa</a></>
+    <>
+      <div className="flex items-center justify-center">
+        <div
+          className="p-8 w-96 border shadow-xl rounded-xl"
+          style={{
+            boxShadow: '0 4px 6px #9088dd, 0 1px 3px rgba(0, 0, 0, 0.1)',
+          }}
+        >
+          <div className="text-center text-2xl mb-6 ">
+            <h1>SmartResto</h1>
+          </div>
+
+          <div>
+            <form action="">
+              <label htmlFor="" className="block text-sm">
+                Username{' '}
+              </label>
+              <input type="text" name="" id="" className="border" />
+
+              <label htmlFor="" className="block mt-4 text-sm">
+                Password{' '}
+              </label>
+
+              <input type="text" name="" id="" className="border" />
+            </form>
+          </div>
+
+          <div
+            className="flex items-center justify-center mt-6 border rounded-lg font-bold"
+            style={{ backgroundColor: '#5a4fcf' }}
+          >
+            <button className="">Login</button>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
